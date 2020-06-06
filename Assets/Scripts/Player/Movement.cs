1acc11a13b841130c283bac20e104fc6f2c18f09
Darren <dnb2094@gmail.com>
2020-06-06 20:05:26 +0100

    0.0.1 - Implemented: Basic Player Movement

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float speed = 1;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private float x;
+    private float y;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        movement = new Vector2(x * speed, y * speed);
+        rb.velocity = movement;
+    }
+}
