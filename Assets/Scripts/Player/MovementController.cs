10c77055c744b90cd9240deb6228e8e2b2afefbe
Darren <dnb2094@gmail.com>
2020-06-08 13:29:02 +0100

    0.0.8 - Implemented: Ground Tiles

---
+++ Assets/Scripts/Player/MovementController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MovementController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private float x;
+    private float y;
+    private int oldX;
+    private int oldY;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        ReadInput();
+        LogPosition((int)transform.position.x, (int)transform.position.y);
+    }
+
+    void FixedUpdate()
+    {
+        movement = new Vector2(x * speed, y * speed);
+        rb.velocity = movement;
+    }
+
+    void OnTriggerExit2D()
+    {
+        Destroy(this.gameObject);
+        Debug.Log($"Destroyed: {this.tag}");
+    }
+
+    private void ReadInput()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void LogPosition(int x, int y)
+    {
+        if(x != oldX || y != oldY)
+        {
+            Debug.Log($"({(int)transform.position.x},{(int)transform.position.y})");
+            oldX = x;
+            oldY = y;
+        }
+    }
+}
