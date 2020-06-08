21e79c646cb8e72e831270cfbf50dfc561ccfd79
Darren <dnb2094@gmail.com>
2020-06-08 13:29:02 +0100

    0.0.8 - Implemented - Ground Tiles

---
+++ Assets/Scripts/CameraDolly.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Transform))]
+public class Dolly : MonoBehaviour
+{
+    [SerializeField] private Transform player = null;
+
+    void Update()
+    {
+        if(player)
+        {
+            transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        }
+    }
+}
