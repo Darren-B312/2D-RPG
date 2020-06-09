7bb0b6d782e30e1249e4399c965545cdb1292500
Darren <dnb2094@gmail.com>
2020-06-09 22:26:02 +0100

    0.0.13 - Implemented: Basic Finite State Machine

---
+++ Assets/IdleState.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : StateMachineBehaviour
+{
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("OnStateEnter: Idle");
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("OnStateUpdate: Idle");
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("OnStateExit: Idle");
+    }
+}
